b0fee89dc21a1c218ffbdb3a4cbe52b9c1a299d4
kakukaracha <100599661+kakukaracha@users.noreply.github.com>
2023-02-28 17:56:13 +0000

    printing and counting

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+    char character;
+    int ainteger;
+    long along;
+    long long alonglong;
+    float afloat;
+
+    printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+    printf("Size of int: %lu byte(s)\n", sizeof(aninteger));
+    printf("Size of long: %lu byte(s)\n", sizeof(along));
+    printf("Size of long long int: %lu byte(s)\n", sizeof(alonglong));
+
+    return (0);
+}
